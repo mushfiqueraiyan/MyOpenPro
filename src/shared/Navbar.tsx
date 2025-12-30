a66a791c255519cb3f5e91a5806b5bc0b73eeebb
mushfiqueraiyan <mushfiqueraiyan1@gmail.com>
2025-12-30 19:18:45 +0600

    Replica of MyOpenPro

---
+++ src/shared/Navbar.tsx
@@ -0,0 +1,58 @@
+import Image from "next/image";
+import { Search, Bell, ChevronDown, Menu } from "lucide-react";
+import logo from "../assests/logo.avif";
+
+export default function Navbar() {
+  return (
+    <div className="relative">
+      <div className="absolute -top-3.5 inset-0 bg-[linear-gradient(to_right,#e5f3f0_1px,transparent_1px),linear-gradient(to_bottom,#e5f3f0_1px,transparent_1px)] bg-size-[40px_40px] -z-10" />
+      <nav className="max-w-7xl mx-auto bg-white border border-gray-200 rounded-4xl mt-5">
+        <div className="max-w-7xl mx-auto px-4">
+          <div className="flex items-center justify-between h-16 gap-4">
+            {/* Left: Logo */}
+            <div className="flex items-center gap-2">
+              <Image src={logo} alt="Open My Pro" width={50} height={50} />
+            </div>
+
+            {/* Center: Search */}
+            <div className="flex-1 max-w-2xl">
+              <div className=" items-center hidden md:flex bg-gray-100 rounded-full px-4 py-2 gap-2">
+                <Menu className="w-5 h-5 text-gray-600" />
+                <select className="bg-transparent outline-none text-sm text-gray-600">
+                  <option>Wellness Pro</option>
+                </select>
+
+                <span className="text-gray-300">|</span>
+
+                <input
+                  type="text"
+                  placeholder="Search Pro Name"
+                  className="flex-1 bg-transparent outline-none text-sm"
+                />
+
+                <Search
+                  size={30}
+                  className=" text-gray-100 rounded-full p-2 bg-black"
+                />
+              </div>
+            </div>
+
+            {/* Right: Actions */}
+            <div className="flex items-center gap-4">
+              <button className="flex items-center gap-2 bg-teal-500 hover:bg-teal-600 text-white px-4 py-2 rounded-full text-sm font-medium">
+                Company
+                <ChevronDown className="w-4 h-4" />
+              </button>
+
+              <Bell className="w-5 h-5 text-gray-600 cursor-pointer" />
+
+              <div className="w-8 h-8 rounded-full bg-teal-500 flex items-center justify-center text-white font-semibold text-sm">
+                G
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
